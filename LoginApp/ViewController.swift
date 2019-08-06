b9a9f3825082bb90ec9b499c2bd0533554da7750
John Heifetz <skidheif@gmail.com>
2019-08-06 09:57:44 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by John Heifetz on 06/08/2019.
+//  Copyright © 2019 heif_team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
